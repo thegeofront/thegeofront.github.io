620dbb289843f974e3df48b56260a91bfe6a7f6d
Jos Feenstra <me@josfeenstra.nl>
2023-05-21 15:01:28 +0200

    add a link, do some styling

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <img src="${viteLogo}" class="logo" alt="Geofront Logo" />
     </a>
     <h1>Geofront</h1>
+    <a href="https://josfeenstra.nl">To personal website</a>
+    <br>
+    <br>
+    <br>
+    <br>
     <p class="more soon">
     🚧 Renovating... 🚧
     </p>
